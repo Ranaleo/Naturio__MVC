3722fb599244d4ac36cf5b630fce9e29e4af310b
User <User@DESKTOP-NRC6TOM>
2024-01-21 02:30:45 +0200

    Add project files.

---
+++ NaturioSkinCare/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NaturioSkinCare.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
